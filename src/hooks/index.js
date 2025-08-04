aab6329100473f5af968d9415d1fdca7d1a82a2a
Paul Grossetête <paulgrossetete.code@gmail.com>
2025-08-04 16:34:40 +0200

    terminé exercice 7.7

---
+++ src/hooks/index.js
@@ -0,0 +1,43 @@
+import  { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+export const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+  const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/name/"
+
+  const fetchCountry = async () => {
+    const countryInfos = await axios
+                          .get(`${baseUrl}${name}`)
+                          .catch(error => ({'status': error.status}))
+    
+    // countryInfos.status === 200 && console.log('successful fetch')
+    // countryInfos.status === 200 && console.log(countryInfos.data)
+    
+    countryInfos.status === 200
+    ? setCountry({
+        found: true,
+        data: countryInfos.data
+      })
+    : setCountry({ found: false })
+  }
+
+  useEffect(() => {
+    name && fetchCountry()
+  }, [name])
+
+  return country
+}
